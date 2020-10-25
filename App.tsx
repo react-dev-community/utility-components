4ffaf836e196918b0a33bad1a9427ce2eef57bfb
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2020-10-25 18:51:37 +0530

    change App.jsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import TxtExample from './examples/txt/TxtExample';
-import VariantTypeFormat from './examples/VariantTypeFormat/VariantTypeFormat';
 import { Container, Txt } from './src';
 
 export default function App() {
   return (
     <Container grow centered>
       <StatusBar hidden />
-      <VariantTypeFormat />
+      <Txt>Hello, World!</Txt>
     </Container>
   );
 }
